5edf2c6a63a856fb9c82cdbeb31ef15a8c03c561
Mohammed Shamsul Arefeen <shamsul.arefeen@industrialskyworks.com>
2021-04-05 14:19:47 -0400

    add coverage settings for jest (commented)

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,26 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    moduleNameMapper: {
-        '\\.css$' : require.resolve('./test/style-mock.js')
+  testEnvironment: "jest-environment-jsdom",
+  moduleNameMapper: {
+    "\\.css$": require.resolve("./test/style-mock.js"),
+  },
+//   coverageThreshold: {
+//     global: {
+//       statements: 30,
+//       branches: 25,
+//       functions: 25,
+//       lines: 20,
+//     },
+//   },
+  collectCoverageFrom: ["**/src/**/*.js", "**/src/**/*.tsx", "**/src/**/*.ts"],
+  moduleDirectories: ["node_modules"],
+  transform: {
+    "\\.tsx?$": "ts-jest",
+    "\\.jsx?$": "babel-jest", // if you have jsx tests too
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: "<rootDir>/tsconfig.json",
     },
-    collectCoverageFrom: ['**/src/**/*.js','**/src/**/*.tsx','**/src/**/*.ts'],
-    moduleDirectories: [
-        'node_modules'
-      ],
-      transform: {
-        "\\.tsx?$": "ts-jest",
-        "\\.jsx?$": "babel-jest", // if you have jsx tests too
-      },
-      globals: {
-        "ts-jest": {
-          "tsconfig": '<rootDir>/tsconfig.json'
-        }
-      },
-      transformIgnorePatterns: [
-        "[/\\\\]node_modules[/\\\\](?!lodash-es/).+\\.js$"
-      ],
-}
+  },
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\](?!lodash-es/).+\\.js$"],
+};
