c7a3f75b377365a76a3a570919d5dd589b37a7cf
Mohammed Shamsul Arefeen <shamsul.arefeen@industrialskyworks.com>
2021-04-07 12:12:43 -0400

    add test wrapper

--- src/__tests__/Bio.js
+++ src/__tests__/Bio.js
@@ -1,35 +1,21 @@
 import React from "react";
 import Bio from "../Bio/Bio";
-
-import { ThemeProvider } from "styled-components/macro";
-import { theme } from "../Style/theme";
 import { render, fireEvent } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import { TestWrapper } from "../utils/TestWrapper";
 
 test("bio front-side renders correctly", () => {
-  const { getByText, debug } = render(
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/">
-        <Bio />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+  const { getByText } = render(<Bio />, { wrapper: TestWrapper });
   const backButton = getByText(/back/i);
   fireEvent.click(backButton);
-  
+
   const welcomeDiv = getByText(/software developer/i);
   expect(welcomeDiv).toBeVisible();
 });
 
 test("test bio flip-side render", () => {
-  const {getByText} = render(
-    <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/">
-        <Bio isFlipped={true}/>
-      </BrowserRouter>
-    </ThemeProvider>
-  )
+  const { getByText } = render(<Bio isFlipped={true} />, {
+    wrapper: TestWrapper,
+  });
   const flipDiv = getByText(/flip side/i);
   expect(flipDiv).toBeVisible();
-
-})
+});
