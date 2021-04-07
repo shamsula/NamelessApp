e9afd9fa5f589c70ad686455fecc15f65ff49717
Mohammed Shamsul Arefeen <shamsul.arefeen@industrialskyworks.com>
2021-04-07 15:37:39 -0400

    simplify tests w/ testing library for cypress

--- cypress/e2e/homepage.js
+++ cypress/e2e/homepage.js
@@ -2,15 +2,17 @@ describe("renders the home page", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  it("renders container correctly", () => {
+  it("renders home content correctly", () => {
+    cy.findByText(/Home/i).should("exist");
     cy.get(".Home__Body-bvxj1u-0").should("exist");
   });
   it("navigate to bio", () => {
-    cy.get('[href="/bio"] > .Button__StyledButton-sc-1906yzf-0').click();
+    // cy.get('[href="/bio"] > .Button__StyledButton-sc-1906yzf-0').click();
+    cy.findByText(/auto-biography/i).click();
     cy.get(".Bio__TextContainer-bqqjlh-1");
   });
   it("navigate to portfolio", () => {
-    cy.get('[href="/portfolio"] > .Button__StyledButton-sc-1906yzf-0').click();
+    cy.findByText(/portfolio/i).click();
     cy.get(".Portfolio__ImagesContainer-sc-1917ty8-0").should("exist");
   });
 });
